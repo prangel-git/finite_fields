542cbebf817ac36bf9deb77d98b1ada41cc5a9b9
prangel-git <62568827+prangel-git@users.noreply.github.com>
2023-08-24 15:53:35 -0400

    playing around with polynomial class
    Co-authored-by: SeanRen01 <SeanRen01@users.noreply.github.com>

--- polynomial.py
+++ polynomial.py
@@ -3,16 +3,44 @@ from itertools import zip_longest
 class Polynomial:
     def __init__(self, coefficients):
         self.coefficients = [c for c in coefficients]
-        self.degree = len(coefficients)
+        erase_leading_zeroes(self.coefficients)
     
     def __call__(self, x):
-        evaluation = [self.coefficients[n] * (x ** n) for n in range(self.degree)]
+        evaluation = [self.coefficients[n] * (x ** n) for n in range(len(self.coefficients))]
         return sum(evaluation)
     
     def __add__(self, other):
         result_coefficients = [a_n + b_n for (a_n, b_n) in zip_longest(self.coefficients, other.coefficients, fillvalue=0)]
         return Polynomial(result_coefficients)
-
-
     
+    def __sub__(self, other):
+        result_coefficients = [a_n - b_n for (a_n, b_n) in zip_longest(self.coefficients, other.coefficients, fillvalue=0)]
+        return Polynomial(result_coefficients)
 
+    def __mul__(self, other):
+        len_a = len(self.coefficients)
+        len_b = len(other.coefficients)
+        a = self.coefficients
+        b = other.coefficients + [0] * len_a
+        product_coefficients = [sum([a[k] * b[n-k] for k in range(min(len_a, n+1))]) for n in range(len_a + len_b - 1)]
+        return Polynomial(product_coefficients)
+    
+    def __mod__(self, other):
+        
+        len_a = len(self.coefficients)
+        len_b = len(other.coefficients)
+        if len_a < len_b:
+            return Polynomial(self.coefficients)
+        
+        factor_coefficients = [0] * (len_a - len_b + 1)
+        factor_coefficients[-1] = self.coefficients[-1] / other.coefficients[-1] 
+        factor = Polynomial(factor_coefficients)
+
+        new_polynomial = self - factor * other
+        return new_polynomial % other
+
+
+def erase_leading_zeroes(coefficients):
+    while len(coefficients) != 0 and coefficients[-1] == 0:
+        coefficients.pop()
+    
--- tests/test_polynomial.py
+++ tests/test_polynomial.py
@@ -2,17 +2,49 @@ import pytest
 
 import polynomial
 
-def test_polynomial_construction():
+def test_construction():
     poly = polynomial.Polynomial(range(2))
     assert poly.coefficients == [0, 1]
 
-def test_evaluate_polynomial():
+def test_leading_zeroes():
+    poly = polynomial.Polynomial([1, 2, 0])
+    assert poly.coefficients == [1, 2]
+
+
+def test_evaluate():
     coeffients = [1, 2]
     poly = polynomial.Polynomial(coeffients)
     assert poly(2) == 5
 
-def test_add_polynomials():
+def test_add():
     poly1 = polynomial.Polynomial([1,2,3])
     poly2 = polynomial.Polynomial([0,1,2,1])
     addition = poly1 + poly2
     assert addition.coefficients == [1, 3, 5, 1]
+
+def test_subtraction():
+    poly1 = polynomial.Polynomial([1,2,3,1])
+    poly2 = polynomial.Polynomial([0,1,2,1])
+    addition = poly1 - poly2
+    assert addition.coefficients == [1, 1, 1]
+    
+
+def test_multiplication():
+    poly1 = polynomial.Polynomial([1,2,1])
+    poly2 = polynomial.Polynomial([0,1,1])
+    addition = poly1 * poly2
+    assert addition.coefficients == [0, 1, 3, 3, 1]
+
+def test_multiplication_2():
+    poly1 = polynomial.Polynomial([0,1])
+    poly2 = polynomial.Polynomial([1,1])
+    addition = poly1 * poly2
+    assert addition.coefficients == [0, 1, 1]
+
+def test_mod():
+    poly1 = polynomial.Polynomial([1,2,1])
+    poly2 = polynomial.Polynomial([1,1])
+    mod = poly1 % poly2
+    assert mod.coefficients == []
+
+
